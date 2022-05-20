50041e2f85b5b2182d3c0a5d9076783a5cab388a
KimTaeHan <82009667+Gaeduck-0908@users.noreply.github.com>
2022-05-20 14:04:16 +0900

    Add files via upload

--- Check.js
+++ Check.js
@@ -2,22 +2,38 @@
 
 function check()
 {
-    if(!document.data.id.value)
+    let id_input = document.data.id;
+    let pw_input = document.data.pw;
+    let pw2_input = document.data.pw2;
+
+    if(!id_input.value)
     {
         alert("ID를 입력해주세요");
-        document.data.id.focus();
+        id_input.focus();
         return false;
     }
-    else if(!document.data.pw.value)
+    else if (id_input.value.length < 6)
+    {
+        alert("ID의 길이가 6보다 작습니다. 6자 이상으로 해주십쇼.");
+        id_input.focus();
+        return false;
+    }
+    else if (id_input.value.length > 15)
+    {
+        alert("ID의 길이가 15보다 큽니다. 15자 이하으로 해주십쇼.");
+        id_input.focus();
+        return false;
+    }
+    else if(!pw_input.value)
     {
         alert("비밀먼호를 입력해주세요");
-        document.data.pw.focus();
+        pw_input.focus();
         return false;
     }
-    else if(!document.data.pw2.value)
+    else if(!pw2_input.value)
     {
         alert("비밀번호 확인을 입력해주세요");
-        document.data.pw2.focus();
+        pw2_input.focus();
         return false;
     }
     else
